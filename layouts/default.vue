b7326039913dc579dabcccc41d2f9f780bad0d33
Boluwatife <jaycewap@gmail.com>
2021-03-10 06:41:55 +0100

    :rocket: Completed homepage Finished with the hompage, yet to add the carousel.

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <section>
+    <div class="header header--secondary">
+      <a href="tel:08035436131" class="secondary__link">
+        <i class="lni lni-phone" />
+        <span>08035436131</span>
+      </a>
+      <a href="mailto:info@laomiesglobal.com" class="secondary__link">
+        <i class="lni lni-envelope" />
+        <span>info@laomiesglobal.com</span>
+      </a>
+      <span class="secondary__link">
+        <i class="lni lni-alarm-clock" />
+        <b>OPEN</b>
+      </span>
+    </div>
+    <input id="hamburger" type="checkbox">
+    <header class="header">
+      <nuxt-link to="/" class="header__link header__link--logo">
+        <img src="~/assets/img/logo.webp" class="header__logo" alt="Laomies Global logo">
+      </nuxt-link>
+      <div class="header__fancy">
+        <nav class="nav">
+          <nuxt-link to="/" class="nav__link" exact-active-class="nav__link--active">
+            home
+          </nuxt-link>
+          <nuxt-link to="/about" class="nav__link" exact-active-class="nav__link--active">
+            about us
+          </nuxt-link>
+          <nuxt-link to="/fleet" class="nav__link" exact-active-class="nav__link--active">
+            fleet
+          </nuxt-link>
+          <nuxt-link to="/career" class="nav__link" exact-active-class="nav__link--active">
+            career
+          </nuxt-link>
+        </nav>
+        <div class="header__social">
+          <a href="//facebook.com/" class="header__link">
+            <i class="lni lni-facebook-filled" />
+          </a>
+          <a href="//twitter.com/" class="header__link">
+            <i class="lni lni-twitter-original" />
+          </a>
+          <a href="//linkedin.com/" class="header__link">
+            <i class="lni lni-linkedin" />
+          </a>
+        </div>
+        <label for="hamburger" class="hamburger" />
+      </div>
+    </header>
+    <Nuxt />
+    <footer class="footer">
+      <div class="row justify-content-between align-items-end m-0">
+        <div class="col-md-4 p-0 p-md-3">
+          <h3 class="heading heading--sub">
+            La'omies Global Marine Services Limited.
+          </h3>
+          <br>
+          <p class="paragraph">
+            KM 22, PortHarcourt Airport road,<br>
+            Igwuruta, PortHarcourt, Rivers State.
+          </p>
+          <div class="mt-3">
+            <a href="tel:08035436131" class="footer__link">08035436131</a>
+            <a href="tel:08075870000" class="footer__link">08075870000</a>
+            <a href="tel:08034362024" class="footer__link">08034362024</a>
+          </div>
+        </div>
+        <div class="col-md-3 p-0 p-md-3 pt-md-5 mt-md-5">
+          <div class="header__social justify-content-end">
+            <a href="//facebook.com/" class="header__link">
+              <i class="lni lni-facebook-filled" />
+            </a>
+            <a href="//twitter.com/" class="header__link">
+              <i class="lni lni-twitter-original" />
+            </a>
+            <a href="//linkedin.com/" class="header__link">
+              <i class="lni lni-linkedin" />
+            </a>
+          </div><br>
+          <p style="text-align: right">
+            &copy; La'omies Limited, 2021
+          </p>
+        </div>
+      </div>
+    </footer>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,207 @@
+<template>
+  <main class="page">
+    <section class="jumbotron">
+      <div class="row align-items-center m-0">
+        <div class="col-md-5 p-0">
+          <h1 class="heading heading--page mb-3">
+            La' omies Global
+          </h1>
+          <p class="paragraph mb-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime reprehenderit corrupti tempora praesentium hic sunt!
+          </p>
+          <div class="d-flex justify-content-between col-md-9 p-0">
+            <a href="#what-we-do" class="btn btn--secondary jumbotron__btn">
+              learn more
+            </a>
+            <nuxt-link to="/contact" class="btn btn--cta jumbotron__btn">
+              get in touch
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="row m-0 pb-5">
+        <div class="col-md-7 px-0 py-3 p-md-3">
+          <h2 class="heading heading--main">
+            About us
+          </h2>
+          <p class="paragraph">
+            La'omies Global Marine Services Limited is an indegenous company, fully registered with the
+            <abbr title="CAC">CAC</abbr>. Our specialization is on the robust marine sector. We provide surveillance and
+            security for multinationals. We also provide procurement, construction, installation, engineering, hiring of equipments and haulage services.
+          </p>
+          <nuxt-link to="/about" class="fancy-link mt-3">
+            Read more
+          </nuxt-link>
+        </div>
+      </div>
+      <div class="row align m-0 mt-5">
+        <div class="col-md-4 px-0 py-3 p-md-3">
+          <div class="card">
+            <div class="card__header">
+              <i class="card__icon lni lni-invention" />
+              <h3 class="card__heading">
+                Vision
+              </h3>
+            </div>
+            <div class="card__body">
+              <p class="card__paragraph">
+                To be a leading indigenous company of choice, prociding world class solution to
+                a variety of problems while carrying out other programs with due diligence.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 px-0 py-3 p-md-3">
+          <div class="card">
+            <div class="card__header">
+              <i class="card__icon lni lni-diamond-alt" />
+              <h3 class="card__heading">
+                Mission
+              </h3>
+            </div>
+            <div class="card__body">
+              <p class="card__paragraph">
+                Business in Nigeria and beyond, committed to values and making impact wherever we go and to be the best in the country.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 px-0 py-3 p-md-3">
+          <div class="card">
+            <div class="card__header">
+              <i class="card__icon lni lni-handshake" />
+              <h3 class="card__heading">
+                Belief
+              </h3>
+            </div>
+            <div class="card__body">
+              <p class="card__paragraph">
+                Our company is therefore, committed to the understanding of today's hardwork and excellent results in success
+                of the future. Hence, our clientele are rest assured of prompt and quality service delivery with results oriented professionals.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="row m-0 pb-5">
+        <div class="col-md-7 px-0 py-3 p-md-3">
+          <h2 class="heading heading--main">
+            Marine security
+          </h2>
+          <p class="paragraph">
+            With over two decades of experience, we have concentrated more on providing oiling,
+            security and surveillance for the oil and gas companies. In the wake of the early 2000s, when the
+            Niger Delta region was largely characterized by violence, was when La'omies Global Marine Services Limited was formed
+            and headed by Mr. Amachree Ibinabo Sonny.
+          </p>
+          <nuxt-link to="/services" class="fancy-link mt-3">
+            Other services
+          </nuxt-link>
+        </div>
+      </div>
+      <div class="row justify-content-around m-0 mt-5">
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--logistics">
+            <h3 class="card__heading">
+              Marine Logistics Services <br>
+              <small>(Vessel chartering)</small>
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--patrol">
+            <h3 class="card__heading">
+              Marine and Offshore security patrol
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--vessel">
+            <h3 class="card__heading">
+              Armed and protective security <br>
+              <small>for all types of vessel</small>
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--port">
+            <h3 class="card__heading">
+              Port security
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--greet">
+            <h3 class="card__heading">
+              Meet 'N' Greet Services
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--catering">
+            <h3 class="card__heading">
+              Offshore catering Services
+            </h3>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="row m-0">
+        <div class="col-md-7 px-0 py-3 p-md-3">
+          <h2 class="heading heading--main">
+            Clients
+          </h2>
+        </div>
+      </div>
+      <div class="row justify-content-around m-0">
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--small card--chevron">
+            <h3 class="card__heading">
+              Chevron Nigeria Limited
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--small card--belema">
+            <h3 class="card__heading">
+              Belema Oil
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--small card--shell">
+            <h3 class="card__heading">
+              The Shell Petroleum Development Company of Nigeria Limited
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--small card--new-cross">
+            <h3 class="card__heading card__heading--bl">
+              New Cross Exploration And Production
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--small card--nnpc">
+            <h3 class="card__heading">
+              Nigeria National Petroleum Corporation
+            </h3>
+          </div>
+        </div>
+        <div class="col-md-3 px-0 py-3 p-md-3">
+          <div class="card card--bg card--small card--delta">
+            <h3 class="card__heading">
+              Niger Delta Development Commission
+            </h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
